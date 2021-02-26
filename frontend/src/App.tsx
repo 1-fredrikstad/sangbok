6e91dee856753542209da0aab0732b632323a95a
amunds1 <andreas.amundsen123@gmail.com>
2021-02-26 13:30:49 +0100

    Activate Chakra in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,19 +6,22 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 import SongView from "./components/pages/SongView";
 import SettingsView from "./components/pages/SettingsView";
-import NavBar from "./components/molecules/NavBar";
+import { CSSReset, ThemeProvider, theme } from "@chakra-ui/react";
 
 const App = () => {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Switch>
-          <Route path="/" exact component={SearchView} />
-          <Route path="/song" component={SongView} />
-          <Route path="/settings" component={SettingsView} />
-          <Route path="/search" component={SearchView} />
-        </Switch>
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CSSReset />
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" exact component={SearchView} />
+            <Route path="/song" component={SongView} />
+            <Route path="/settings" component={SettingsView} />
+            <Route path="/search" component={SearchView} />
+          </Switch>
+        </BrowserRouter>
+      </ThemeProvider>
     </div>
   );
 };
