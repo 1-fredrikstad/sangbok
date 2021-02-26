94d66f914a7b5115107505ef0840fed19978a334
amunds1 <andreas.amundsen123@gmail.com>
2021-02-26 13:24:44 +0100

    Add routes to root component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,26 @@
 import React from "react";
-import SearchView from "./components/pages/SearchView";
 
-function App() {
+import SearchView from "./components/pages/SearchView";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import "./App.css";
+import SongView from "./components/pages/SongView";
+import SettingsView from "./components/pages/SettingsView";
+import NavBar from "./components/molecules/NavBar";
+
+const App = () => {
   return (
     <div className="App">
-      <SearchView />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={SearchView} />
+          <Route path="/song" component={SongView} />
+          <Route path="/settings" component={SettingsView} />
+          <Route path="/search" component={SearchView} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
