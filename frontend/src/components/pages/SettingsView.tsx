16a6ba425a0b2d5fa0d021ce3b0f0f6f3fe115bc
amunds1 <andreas.amundsen123@gmail.com>
2021-02-26 14:32:12 +0100

    Add demo to SettingsView

--- frontend/src/components/pages/SettingsView.tsx
+++ frontend/src/components/pages/SettingsView.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 const SettingsView = () => {
-  return <div></div>;
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (lng: string) => {
+    i18n.changeLanguage(lng);
+  };
+  return (
+    <div>
+      <div>{t("settingsHeader")}</div>
+      <button type="button" onClick={() => changeLanguage("en")}>
+        en
+      </button>
+      <button type="button" onClick={() => changeLanguage("no")}>
+        no
+      </button>
+    </div>
+  );
 };
 
 export default SettingsView;
