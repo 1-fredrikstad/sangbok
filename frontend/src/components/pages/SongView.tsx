2160f1d50a4c910bab70793ed56bc3f48bcd12e9
amunds1 <andreas.amundsen123@gmail.com>
2021-02-27 14:49:44 +0100

    Fetch songs from Sanity and display in frontend

--- frontend/src/components/pages/SongView.tsx
+++ frontend/src/components/pages/SongView.tsx
@@ -1,13 +1,32 @@
 import React from "react";
 import NavBar from "../molecules/NavBar";
 
+import { gql, useQuery } from "@apollo/client";
+import { Skeleton, Text } from "@chakra-ui/react";
+import SongTemplate from "../templates/SongTemplate";
+
+const GET_SONGS = gql`
+  query GetSongs {
+    allSong {
+      title
+      verses
+      melody
+      author
+    }
+  }
+`;
+
 const SongView = () => {
-  return (
-    <div>
-      <h1>Song</h1>
-      <NavBar />
-    </div>
-  );
+  const { loading, error, data } = useQuery(GET_SONGS);
+  console.log(data);
+
+  if (loading) return <Skeleton height="500px" />;
+  if (error || !data) {
+    console.log(error);
+    return <Text>Lol du har feil</Text>;
+  }
+
+  return <SongTemplate songs={data.allSong} />;
 };
 
 export default SongView;
