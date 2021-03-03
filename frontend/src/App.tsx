d91a586f1d54cec291f6d4032c44bee851b9baab
kharann <anhkhav@gmail.com>
2021-03-03 09:18:40 +0100

    Finish up linting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,28 @@
 import React from 'react';
+import { ChakraProvider, Switch, theme } from '@chakra-ui/react';
+import { ApolloProvider } from '@apollo/client';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import SongView from './components/pages/SongView';
 import SettingsView from './components/pages/SettingsView';
-import { ChakraProvider, Switch, theme } from '@chakra-ui/react';
-import { ApolloProvider } from '@apollo/client';
 import { client } from './api/apolloClient';
-import { BrowserRouter, Route } from 'react-router-dom';
 import SearchView from './components/pages/SearchView';
 
-const App = () => {
-  return (
-    <div className="App">
-      <ApolloProvider client={client}>
-        <ChakraProvider theme={theme}>
-          <BrowserRouter>
-            <Switch>
-              <Route path="/" exact component={SearchView} />
-              <Route path="/song" component={SongView} />
-              <Route path="/settings" component={SettingsView} />
-              <Route path="/search" component={SearchView} />
-            </Switch>
-          </BrowserRouter>
-        </ChakraProvider>
-      </ApolloProvider>
-    </div>
-  );
-};
+const App = () => (
+  <div className="App">
+    <ApolloProvider client={client}>
+      <ChakraProvider theme={theme}>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" exact component={SearchView} />
+            <Route path="/song" component={SongView} />
+            <Route path="/settings" component={SettingsView} />
+            <Route path="/search" component={SearchView} />
+          </Switch>
+        </BrowserRouter>
+      </ChakraProvider>
+    </ApolloProvider>
+  </div>
+);
 
 export default App;
