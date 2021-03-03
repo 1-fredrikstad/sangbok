b5aaf03e9c509f2cae9260dc48c469b79a106ab0
Magnus Lauritzen Holtet <magnulho@gmail.com>
2021-03-03 10:54:58 +0100

    Configure base layout.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
+import { Flex } from "@chakra-ui/react";
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import NavBar from "./components/molecules/NavBar";
 import SearchView from "./components/pages/SearchView";
 import SettingsView from "./components/pages/SettingsView";
 import SongView from "./components/pages/SongView";
@@ -7,12 +9,21 @@ import SongView from "./components/pages/SongView";
 const App = () => {
   return (
     <BrowserRouter>
+      {/* HEADER LIGGER HER OG OPPDATERES */}
+
+      {/* CONTENT */}
       <Switch>
-        <Route path="/" exact component={SearchView} />
-        <Route path="/song" component={SongView} />
-        <Route path="/settings" component={SettingsView} />
-        <Route path="/search" component={SearchView} />
+        <Flex overflow="scroll" height="100%">
+          <Route path="/" exact component={SearchView} />
+          <Route path="/song" component={SongView} />
+          <Route path="/settings" component={SettingsView} />
+          <Route path="/search" component={SearchView} />
+        </Flex>
       </Switch>
+
+      <Flex>
+        <NavBar />
+      </Flex>
     </BrowserRouter>
   );
 };
