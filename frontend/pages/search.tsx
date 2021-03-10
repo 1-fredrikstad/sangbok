0cf6b3726ec30bc5b93e46e7e8ed8de2c17958bd
kharann <anhkhav@gmail.com>
2021-03-10 12:12:04 +0100

    Move searchview to pages

--- frontend/pages/search.tsx
+++ frontend/pages/search.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
-import SearchView from '@components/pages/SearchView';
 import { NextPage } from 'next';
+import { Flex } from '@chakra-ui/react';
+import SearchInput from '@components/atoms/SearchInput';
+import Header from '@components/molecules/Header';
+import SongView from '@components/pages/SongView';
 
-const Search: NextPage = () => <SearchView />;
+const Search: NextPage = () => (
+  <Flex flexDir="column" w="100%">
+    <Header color="#FFD687">
+      <SearchInput />
+    </Header>
+    <SongView />
+  </Flex>
+);
 
 export default Search;
