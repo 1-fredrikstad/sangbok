3ed9089f856684ffc44f87e0078461dac55355e1
Magnus Lauritzen Holtet <magnulho@gmail.com>
2021-03-10 10:44:42 +0100

    Pretty linting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,23 +7,23 @@ import SettingsView from './components/pages/SettingsView';
 import SongView from './components/pages/SongView';
 
 const App = () => (
-    <BrowserRouter>
-      {/* HEADER LIGGER HER OG OPPDATERES */}
+  <BrowserRouter>
+    {/* HEADER LIGGER HER OG OPPDATERES */}
 
-      {/* CONTENT */}
-      <Switch>
-        <Flex overflow="scroll" height="100%">
-          <Route path="/" exact component={SearchView} />
-          <Route path="/song" component={SongView} />
-          <Route path="/settings" component={SettingsView} />
-          <Route path="/search" component={SearchView} />
-        </Flex>
-      </Switch>
-
-      <Flex>
-        <NavBar />
+    {/* CONTENT */}
+    <Switch>
+      <Flex overflow="scroll" height="100%">
+        <Route path="/" exact component={SearchView} />
+        <Route path="/song" component={SongView} />
+        <Route path="/settings" component={SettingsView} />
+        <Route path="/search" component={SearchView} />
       </Flex>
-    </BrowserRouter>
-  );
+    </Switch>
+
+    <Flex>
+      <NavBar />
+    </Flex>
+  </BrowserRouter>
+);
 
 export default App;
--- frontend/src/components/atoms/SearchInput.tsx
+++ frontend/src/components/atoms/SearchInput.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
-import { SearchIcon } from "@chakra-ui/icons";
+import React from 'react';
+import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
+import { SearchIcon } from '@chakra-ui/icons';
 
 const SearchInput = () => (
-    <InputGroup width="20rem">
-      <InputLeftElement pointerEvents="none" children={<SearchIcon />} />
-      <Input bg="white" />
-    </InputGroup>
-  );
+  <InputGroup width="20rem">
+    <InputLeftElement pointerEvents="none" children={<SearchIcon />} />
+    <Input bg="white" />
+  </InputGroup>
+);
 
 export default SearchInput;
