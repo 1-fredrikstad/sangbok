bc5d807b6f113b61394ef9ae11de53c613ef2836
kharann <anhkhav@gmail.com>
2021-03-15 18:17:48 +0100

    Refactor navbar

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,7 +1,48 @@
 import React from "react";
 import { NextPage } from "next";
 import Search from "./search";
+import Song from "./song";
+import Layout from "@components/templates/layout";
 
-const Home: NextPage = () => <Search />;
+const Home: NextPage = () => (
+    <Layout>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+            molestiae libero voluptatibus itaque quidem ab fugit, iusto
+            obcaecati tenetur pariatur illum impedit saepe blanditiis facere
+            veritatis quaerat nulla, quis odit!
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+            molestiae libero voluptatibus itaque quidem ab fugit, iusto
+            obcaecati tenetur pariatur illum impedit saepe blanditiis facere
+            veritatis quaerat nulla, quis odit!
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+            molestiae libero voluptatibus itaque quidem ab fugit, iusto
+            obcaecati tenetur pariatur illum impedit saepe blanditiis facere
+            veritatis quaerat nulla, quis odit!
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+            molestiae libero voluptatibus itaque quidem ab fugit, iusto
+            obcaecati tenetur pariatur illum impedit saepe blanditiis facere
+            veritatis quaerat nulla, quis odit!
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+            molestiae libero voluptatibus itaque quidem ab fugit, iusto
+            obcaecati tenetur pariatur illum impedit saepe blanditiis facere
+            veritatis quaerat nulla, quis odit!
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+            molestiae libero voluptatibus itaque quidem ab fugit, iusto
+            obcaecati tenetur pariatur illum impedit saepe blanditiis facere
+            veritatis quaerat nulla, quis odit!
+        </p>
+    </Layout>
+);
 
 export default Home;
---
+++ frontend/src/components/templates/layout.tsx
@@ -0,0 +1,14 @@
+import { Stack } from "@chakra-ui/react";
+import NavBar from "@components/molecules/NavBar";
+import React, { FC } from "react";
+
+const Layout: FC<{}> = ({ children }) => {
+    return (
+        <Stack as="main" minHeight="100vh">
+            {children}
+            <NavBar />
+        </Stack>
+    );
+};
+
+export default Layout;
