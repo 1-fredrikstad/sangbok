82fa83bff1fb0eead84af7256ec7ac08b04cc2c8
kharann <anhkhav@gmail.com>
2021-03-17 13:35:47 +0100

    Update songtitlequery with slug and rename it

--- frontend/src/components/organisms/SongList.tsx
+++ frontend/src/components/organisms/SongList.tsx
@@ -1,16 +1,16 @@
 import { useQuery } from "@apollo/client";
 import { Link, Skeleton, Stack } from "@chakra-ui/react";
-import { GetSongNames } from "@graphqlTypes/GetSongNames";
+import { GetSongTitleQuery } from "@graphqlTypes/GetSongTitleQuery";
 import React, { FC } from "react";
-import { SongNameQuery } from "src/api/songs";
+import { SONG_TITLE_QUERY } from "src/api/songs";
 
 const SongList: FC = () => {
-  const { data, loading } = useQuery<GetSongNames>(SongNameQuery);
+  const { data, loading } = useQuery<GetSongTitleQuery>(SONG_TITLE_QUERY);
   if (loading) return <Skeleton w="100%" h="100%" />;
   return (
     <Stack>
-      {data.allSong.map((song) => (
-        <Link href={`song/${song.numbering}`} key={song.title}>
+      {data.songs.map((song) => (
+        <Link href={`song/${song.slug.current}`} key={song.title}>
           {song.title}
         </Link>
       ))}
