b7ce15171db1f21f011382c53409d098047d9d8b
Kristoffer <kristnyv@stud.ntnu.no>
2021-03-22 23:25:57 +0100

    add melody to props, pass to textfield in the FC. Rendered as text

--- frontend/src/components/molecules/SongDetails.tsx
+++ frontend/src/components/molecules/SongDetails.tsx
@@ -3,9 +3,10 @@ import React, { FC } from "react";
 
 interface SongDetailsProps {
   title: string;
-  author: string
+  author: string;
+  melody: string
 }
-const SongDetails: FC<SongDetailsProps> = ({ title, author }) => {
+const SongDetails: FC<SongDetailsProps> = ({ title, author, melody }) => {
   return (
     <Box p="2.5rem 2.5rem 1.5rem">
       <Heading fontWeight="300" as="h1" size="xl">
@@ -13,6 +14,9 @@ const SongDetails: FC<SongDetailsProps> = ({ title, author }) => {
       </Heading>
       <Text fontSize="sm" fontWeight="light">
         Forfatter: {author}
+      </Text>
+      <Text fontSize="sm" fontWeight="light">
+        Melodi: {melody}
       </Text>
     </Box>
   );
