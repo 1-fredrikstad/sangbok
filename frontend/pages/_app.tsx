efef5f432c89ec9c7173abbc28209fd50bcd9c88
kharann <anhkhav@gmail.com>
2021-03-24 10:46:54 +0100

    Remove all apollo related

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,21 +2,15 @@ import React from "react";
 import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "@definitions/chakra/theme";
-import { initializeApollo } from "@services/graphql";
-import { ApolloProvider } from "@apollo/client";
 import { appWithTranslation } from "next-i18next";
 import { SlugProvider } from "@services/context/SlugProvider";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  const apolloClient = initializeApollo();
-
   return (
     <ChakraProvider theme={theme}>
-      <ApolloProvider client={apolloClient}>
-        <SlugProvider>
-          <Component {...pageProps} />
-        </SlugProvider>
-      </ApolloProvider>
+      <SlugProvider>
+        <Component {...pageProps} />
+      </SlugProvider>
     </ChakraProvider>
   );
 }
