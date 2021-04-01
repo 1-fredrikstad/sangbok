fa4a32f5bfde4b7125a62e0dbc47e2fc94bb0cdc
Andreas Amundsen <andreas.amundsen123@gmail.com>
2021-04-01 13:43:16 +0200

    Lint

--- frontend/src/components/molecules/VerseChorusNumbering.tsx
+++ frontend/src/components/molecules/VerseChorusNumbering.tsx
@@ -8,22 +8,18 @@ interface VerseChorusNumberingProps {
 
 const VerseChorusNumbering: FC<VerseChorusNumberingProps> = ({
   song: { verses, chorus },
-}) => {
-  return (
-    <div>
-      <Stack spacing={6} h="100%">
-        {verses.map((verse, i) => (
-          <>
-            <Text key={`verse${i}`} whiteSpace="pre-wrap">
-              {`${i + 1}. ${verse}`}
-            </Text>
+}) => (
+  <Stack spacing={6} h="100%">
+    {verses.map((verse, i) => (
+      <>
+        <Text key={`verse${i}`} whiteSpace="pre-wrap">
+          {`${i + 1}. ${verse}`}
+        </Text>
 
-            {i === 0 && <Text>{`Ref: ${chorus}`}</Text>}
-          </>
-        ))}
-      </Stack>
-    </div>
-  );
-};
+        {i === 0 && <Text>{`Ref: ${chorus}`}</Text>}
+      </>
+    ))}
+  </Stack>
+);
 
 export default VerseChorusNumbering;
