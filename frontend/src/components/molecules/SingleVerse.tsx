b06333ef931375fb70eed38bd5450fd794317e8d
Andreas Amundsen <andreas.amundsen123@gmail.com>
2021-04-01 13:42:41 +0200

    Lint

--- frontend/src/components/molecules/SingleVerse.tsx
+++ frontend/src/components/molecules/SingleVerse.tsx
@@ -6,22 +6,10 @@ interface SingleVerseProps {
   song: SongDetailQuery_details;
 }
 
-const SongDetail: FC<SingleVerseProps> = ({ song: { verses } }) => {
-  return (
-    <>
-      <Box h="100%">
-        <Text whiteSpace="pre-wrap">{verses[0]}</Text>
-      </Box>
-    </>
-  );
-};
+const SongDetail: FC<SingleVerseProps> = ({ song: { verses } }) => (
+  <Box h="100%">
+    <Text whiteSpace="pre-wrap">{verses[0]}</Text>
+  </Box>
+);
 
 export default SongDetail;
-
-// <Stack spacing={6} h="100%">
-// {verses.map((verse, i) => (
-//   <Text key={`verse${i}`} whiteSpace="pre-wrap">
-//     {verse}
-//   </Text>
-// ))}
-// </Stack>
