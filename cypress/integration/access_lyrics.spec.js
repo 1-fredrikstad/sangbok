8a9084801d93aa3d8d8fbc85bc9355cd7217d0f4
Andreas Amundsen <andreas.amundsen123@gmail.com>
2021-04-10 10:06:05 +0200

    Add access lyrics integration test

---
+++ cypress/integration/access_lyrics.spec.js
@@ -0,0 +1,9 @@
+describe("Access lyrics", () => {
+  it("Select a song", () => {
+    cy.visit("http://localhost:3000/search")
+    /* ==== Generated with Cypress Studio ==== */
+    cy.get('[href="song/enda-en-test-sang"]').click();
+    /* ==== End Cypress Studio ==== */
+  })
+})
+
