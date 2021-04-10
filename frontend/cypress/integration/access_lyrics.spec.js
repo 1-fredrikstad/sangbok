49ad9939ef29de393f0506954fbe5ed1119b7b76
Andreas Amundsen <andreas.amundsen123@gmail.com>
2021-04-10 11:21:14 +0200

    Access lyrics spec now selects first song in list

--- frontend/cypress/integration/access_lyrics.spec.js
+++ frontend/cypress/integration/access_lyrics.spec.js
@@ -1,9 +1,12 @@
 describe("Access lyrics", () => {
   it("Select and view a song", () => {
     cy.visit("http://localhost:3000/search")
-    /* ==== Generated with Cypress Studio ==== */
-    cy.get('[href="song/enda-en-test-sang"]').click();
-    /* ==== End Cypress Studio ==== */
+    
+    /* Select the first song from list of songs */
+    cy.get(".css-1sjndut").within(() => {
+      cy.get('a').first().click();
+    });
+    
   })
 })
 
