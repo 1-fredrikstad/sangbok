455d315772dd0b45b0e3960bafad699aa44a78b1
Magnus Lauritzen Holtet <magnulho@gmail.com>
2021-04-10 10:59:37 +0200

    Change base layout scalings.

--- frontend/src/components/templates/Layout.tsx
+++ frontend/src/components/templates/Layout.tsx
@@ -1,13 +1,15 @@
-import { Stack } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 import NavBar from "@components/molecules/NavBar";
 import React, { FC } from "react";
 
 const Layout: FC = ({ children }) => {
   return (
-    <Stack as="main" h="100vh">
-      {children}
+    <Flex as="main" h="100vh" direction="column">
+      <Flex direction="column" flex="12" overflow="scroll">
+        {children}
+      </Flex>
       <NavBar />
-    </Stack>
+    </Flex>
   );
 };
 
