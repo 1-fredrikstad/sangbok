c43615ada91722be5f86cab5ea527ad93bb9e314
Andreas Amundsen <andreas.amundsen123@gmail.com>
2021-04-11 11:22:10 +0200

    Change /search to /index

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,45 +1,45 @@
+import { Box, Center } from "@chakra-ui/react";
+import SearchInput from "@components/atoms/SearchInput";
+import Header from "@components/molecules/Header";
+import SongList from "@components/organisms/SongList";
 import Layout from "@components/templates/Layout";
+import client from "@services/groq/client";
 import { NextPage } from "next";
-import React from "react";
+import React, { useState } from "react";
+import { ChangeEvent } from "react";
+import { SongDetailType, SongListEntry, SONG_LIST_QUERY } from "src/api/songs";
+interface SearchProps {
+  songs: SongListEntry[];
+}
 
-const Home: NextPage = () => (
-  <Layout>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
-      molestiae libero voluptatibus itaque quidem ab fugit, iusto obcaecati
-      tenetur pariatur illum impedit saepe blanditiis facere veritatis quaerat
-      nulla, quis odit!
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
-      molestiae libero voluptatibus itaque quidem ab fugit, iusto obcaecati
-      tenetur pariatur illum impedit saepe blanditiis facere veritatis quaerat
-      nulla, quis odit!
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
-      molestiae libero voluptatibus itaque quidem ab fugit, iusto obcaecati
-      tenetur pariatur illum impedit saepe blanditiis facere veritatis quaerat
-      nulla, quis odit!
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
-      molestiae libero voluptatibus itaque quidem ab fugit, iusto obcaecati
-      tenetur pariatur illum impedit saepe blanditiis facere veritatis quaerat
-      nulla, quis odit!
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
-      molestiae libero voluptatibus itaque quidem ab fugit, iusto obcaecati
-      tenetur pariatur illum impedit saepe blanditiis facere veritatis quaerat
-      nulla, quis odit!
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
-      molestiae libero voluptatibus itaque quidem ab fugit, iusto obcaecati
-      tenetur pariatur illum impedit saepe blanditiis facere veritatis quaerat
-      nulla, quis odit!
-    </p>
-  </Layout>
-);
-export default Home;
+const Search: NextPage<SearchProps> = ({ songs }) => {
+  const [searchValue, setSearchValue] = useState("");
+  const handleSearchChange = (event: ChangeEvent<HTMLInputElement>) =>
+    setSearchValue((event.target as HTMLInputElement).value);
+
+  const filteredSongs = songs.filter((song: SongDetailType) =>
+    song.title.toLowerCase().includes(searchValue.toLowerCase()),
+  );
+
+  return (
+    <Layout>
+      <Header color="#FFD687">
+        <Center pt="2rem">
+          <SearchInput value={searchValue} handleChange={handleSearchChange} />
+        </Center>
+      </Header>
+      <Box padding="5">
+        <SongList songs={filteredSongs} />
+      </Box>
+    </Layout>
+  );
+};
+
+Search.getInitialProps = async (_ctx) => {
+  const data = await client.fetch<SongListEntry[]>(SONG_LIST_QUERY);
+  return {
+    songs: data,
+  };
+};
+
+export default Search;
