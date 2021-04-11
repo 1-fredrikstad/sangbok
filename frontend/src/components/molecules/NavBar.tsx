e9986bb63bce0bf61e724bbd289b9bf0ec557612
Andreas Amundsen <andreas.amundsen123@gmail.com>
2021-04-11 11:15:05 +0200

    Change background color to white and add borderTop

--- frontend/src/components/molecules/NavBar.tsx
+++ frontend/src/components/molecules/NavBar.tsx
@@ -9,20 +9,14 @@ const NavBar: FC = () => {
 
   return (
     <Flex
-      bg="gray.300"
+      bg="white"
       flex="1"
-      // alignSelf="flex-end"
       alignItems="center"
       justifyContent="space-around"
-      // maxHeight="16vh"
       height="3.5rem"
       width="100%"
-      // position="fixed"
-      // left="0"
-      // right="0"
-      // bottom="0"
       zIndex="1"
-      // marginTop="2"
+      borderTop="1px solid grey"
     >
       <IconButton
         variant="outline"
