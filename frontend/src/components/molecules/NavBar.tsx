95abf22422782f5e8fb3517e110088768174eba4
kharann <anhkhav@gmail.com>
2021-04-13 16:14:09 +0200

    Change navbar to be fixed

--- frontend/src/components/molecules/NavBar.tsx
+++ frontend/src/components/molecules/NavBar.tsx
@@ -1,39 +1,42 @@
 import React, { FC } from "react";
 
-import { Flex, IconButton } from "@chakra-ui/react";
+import { Box, Flex, IconButton, useColorMode } from "@chakra-ui/react";
 import { FiSettings, FiSearch } from "react-icons/fi";
 import { useRouter } from "next/dist/client/router";
 
 const NavBar: FC = () => {
   const router = useRouter();
+  const { colorMode } = useColorMode();
 
   return (
-    <Flex
-      flex="1"
-      alignItems="center"
-      justifyContent="space-around"
-      height="3.5rem"
+    <Box
+      position="fixed"
+      bottom="0"
       width="100%"
-      zIndex="1"
       borderTop="1px solid grey"
+      height="3.5rem"
+      zIndex="1"
+      bg={colorMode === "light" ? "besh.50" : "gray.800"}
     >
-      <IconButton
-        variant="outline"
-        border="none"
-        size="lg"
-        onClick={() => router.push("/")}
-        icon={<FiSearch />}
-        aria-label="Go to search view"
-      />
-      <IconButton
-        variant="outline"
-        border="none"
-        size="lg"
-        onClick={() => router.push("/settings")}
-        icon={<FiSettings />}
-        aria-label="Go to settings view"
-      />
-    </Flex>
+      <Flex flex="1" alignItems="center" justifyContent="space-around">
+        <IconButton
+          variant="outline"
+          border="none"
+          size="lg"
+          onClick={() => router.push("/")}
+          icon={<FiSearch />}
+          aria-label="Go to search view"
+        />
+        <IconButton
+          variant="outline"
+          border="none"
+          size="lg"
+          onClick={() => router.push("/settings")}
+          icon={<FiSettings />}
+          aria-label="Go to settings view"
+        />
+      </Flex>
+    </Box>
   );
 };
 
