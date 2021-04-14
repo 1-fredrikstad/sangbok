54174a7b6b6b7e5f4db04cb7b9e7a960f2765fcb
Magnus Lauritzen Holtet <magnulho@gmail.com>
2021-04-14 13:31:59 +0200

    Header adjustment

--- frontend/src/components/templates/Layout.tsx
+++ frontend/src/components/templates/Layout.tsx
@@ -2,12 +2,13 @@ import { Flex, useColorMode } from '@chakra-ui/react';
 import NavBar from '@components/molecules/NavBar';
 import React, { FC } from 'react';
 
+// 60px is navbar height
 const Layout: FC = ({ children }) => {
   const { colorMode } = useColorMode();
   return (
     <Flex
       as="main"
-      h="100vh"
+      h="calc(100vh - 60px)"
       direction="column"
       bg={colorMode === 'light' ? 'besh.50' : 'gray.800'}
       marginBottom="60px"
