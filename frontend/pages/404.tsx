76bda7faa135686785ca3b6e350e9650a9e696d7
kharann <anhkhav@gmail.com>
2021-04-17 13:20:39 +0200

    lint format 404 page

--- frontend/pages/404.tsx
+++ frontend/pages/404.tsx
@@ -1,8 +1,8 @@
-import Layout from "@components/templates/Layout";
-import React from "react";
-import { Heading } from "@chakra-ui/react";
-import Image from "next/image";
-import { NextPage } from "next";
+import Layout from '@components/templates/Layout';
+import React from 'react';
+import { Heading } from '@chakra-ui/react';
+import Image from 'next/image';
+import { NextPage } from 'next';
 
 const NotFound404: NextPage = () => {
   return (
@@ -10,12 +10,7 @@ const NotFound404: NextPage = () => {
       <Heading as="h4" padding="10">
         Oisann! Sangen er ikke funnet
       </Heading>
-      <Image
-        src="/images/404error.svg"
-        alt="No song found"
-        width={500}
-        height={500}
-      />
+      <Image src="/images/404error.svg" alt="No song found" width={500} height={500} />
     </Layout>
   );
 };
