69efe192fe4d350015c920f9953a8abe39a17473
Andreas Amundsen <andreas.amundsen123@gmail.com>
2021-04-20 14:38:54 +0200

    Add shallow test for ThemeToggler

--- frontend/src/__tests__/ThemeToggler.spec.tsx
+++ frontend/src/__tests__/ThemeToggler.spec.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import ThemeToggler from '../components/atoms/ThemeToggler';
 
-describe('Pages', () => {
+describe('ThemeToggler', () => {
   const wrapper = shallow(<ThemeToggler />);
 
   it('should render without throwing an error', () => {
