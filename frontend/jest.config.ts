3b8763a219c560783aa3d9a0d9dfbbce96244244
carlofholy <carl.smestad@gmail.com>
2021-04-21 13:35:38 +0200

    setup jest

---
+++ frontend/jest.config.ts
@@ -0,0 +1,8 @@
+import type {Config} from '@jest/types';
+
+const config: Config.InitialOptions = {
+  verbose: true,
+  setupFilesAfterEnv: ['./jest.setup.ts'],
+  testPathIgnorePatterns: ['./.next/', './node_modules/', './cypress/'],
+};
+export default config;
