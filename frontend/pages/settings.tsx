e384d75c081bf37ad12ceb97505231112866350a
Petter Charles Stellberg Redfern <petter.charles@redfern.no>
2021-04-21 10:39:52 +0200

    Add logo to header and settings

--- frontend/pages/settings.tsx
+++ frontend/pages/settings.tsx
@@ -1,4 +1,4 @@
-import { Heading, Box, Image, Divider, Center, useColorMode } from '@chakra-ui/react';
+import { Heading, Box, Image, Divider, Center, useColorMode, Flex, Link } from '@chakra-ui/react';
 import ThemeToggler from '@components/atoms/ThemeToggler';
 import Header from '@components/molecules/Header';
 import Layout from '@components/templates/Layout';
@@ -18,6 +18,24 @@ const SettingsView: FC = () => {
         <ThemeToggler />
         <Divider size="L" mt="1rem" mb="1rem" />
       </Box>
+      <Flex justifyContent="space-between">
+        <div>
+          <img
+            src={colorMode === 'light' ? 'images/logo.gif' : 'images/logo-white.gif'}
+            alt="1. Fredrikstad speidergruppes logo"
+            width="100px"
+            height="120px"
+          />
+        </div>
+        <p style={{ maxWidth: '60%' }}>
+          Appen er laget for 1. Fredrikstad speidergruppe som et bachelorprosjekt av studenter ved NTNU. For mer info om
+          prosjektet se
+          <Link href="https://github.com/1-fredrikstad/sangbok/" style={{ textDecoration: 'underline' }}>
+            {' '}
+            GitHub.
+          </Link>
+        </p>
+      </Flex>
       <Center height="100%">
         <Image
           src={colorMode === 'light' ? '/images/camping.svg' : '/images/camping_dark.svg'}
