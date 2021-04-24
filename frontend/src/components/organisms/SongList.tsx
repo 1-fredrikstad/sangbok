e5a1f720844c1338b06de1a7fc61021adba85c80
Andreas Amundsen <andreas.amundsen123@gmail.com>
2021-04-24 15:11:33 +0200

    Remove console.log

--- frontend/src/components/organisms/SongList.tsx
+++ frontend/src/components/organisms/SongList.tsx
@@ -14,40 +14,37 @@ const SongList: FC<SongListProps> = ({ songs }) => {
 
   return (
     <Stack spacing={6}>
-      {songs.map((song) => {
-        console.log(song);
-        return (
-          <NextLink href={`song/${song.slug.current}`} key={song.title}>
-            <Link
-              display="flex"
-              shadow="sm"
-              justifyContent="space-between"
-              padding="1"
-              textDecoration="no"
-              _active={{ textDecoration: 'none' }}
-              _hover={{ textDecoration: 'none' }}
-            >
-              <Stack key={song.slug.current} maxWidth="95%">
-                <Text isTruncated>
-                  {song.order + 1}. {song.title}
-                </Text>
-                <Flex alignItems="center" color={colorMode === 'light' ? 'green.700' : 'green.200'}>
-                  {song.melody && (
-                    <>
-                      <Icon as={FaItunesNote} marginRight="2" />
-                      {song.melody}
-                    </>
-                  )}
-                </Flex>
-              </Stack>
-              <HStack>
-                {song.audioUrl && <ImFileMusic size="21px" color={colorMode === 'light' ? '#191414' : 'white'} />}
-                {song.spotifyuri && <FaSpotify size="21px" color={colorMode === 'light' ? '#191414' : 'white'} />}
-              </HStack>
-            </Link>
-          </NextLink>
-        );
-      })}
+      {songs.map((song) => (
+        <NextLink href={`song/${song.slug.current}`} key={song.title}>
+          <Link
+            display="flex"
+            shadow="sm"
+            justifyContent="space-between"
+            padding="1"
+            textDecoration="no"
+            _active={{ textDecoration: 'none' }}
+            _hover={{ textDecoration: 'none' }}
+          >
+            <Stack key={song.slug.current} maxWidth="95%">
+              <Text isTruncated>
+                {song.order + 1}. {song.title}
+              </Text>
+              <Flex alignItems="center" color={colorMode === 'light' ? 'green.700' : 'green.200'}>
+                {song.melody && (
+                  <>
+                    <Icon as={FaItunesNote} marginRight="2" />
+                    {song.melody}
+                  </>
+                )}
+              </Flex>
+            </Stack>
+            <HStack>
+              {song.audioUrl && <ImFileMusic size="21px" color={colorMode === 'light' ? '#191414' : 'white'} />}
+              {song.spotifyuri && <FaSpotify size="21px" color={colorMode === 'light' ? '#191414' : 'white'} />}
+            </HStack>
+          </Link>
+        </NextLink>
+      ))}
     </Stack>
   );
 };
