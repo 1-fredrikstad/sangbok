9e2c3fe36f59d9c4154c2b7b9665657461892c7d
Andreas Amundsen <andreas.amundsen123@gmail.com>
2021-04-26 15:08:23 +0200

    Fix activate darkmode integration test

--- frontend/cypress/integration/activate_darkmode.spec.js
+++ frontend/cypress/integration/activate_darkmode.spec.js
@@ -9,6 +9,11 @@ describe('Activate darkmode', () => {
   it('Navigate to settings and activate darkmode', function () {
     cy.visit('localhost:3000/');
 
-    
+
+    /* ==== Generated with Cypress Studio ==== */
+    cy.get('[aria-label="Go to settings view"] > svg').click();
+    cy.get('.chakra-switch__thumb').click();
+    cy.get('#dark-mode').check();
+    /* ==== End Cypress Studio ==== */
   });
 });
